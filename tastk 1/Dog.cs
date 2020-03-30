3a8c9efeedd83cbe3006658c66caeb8184633bbd
Ivica janevski <janevski.ivica@outlook.com>
2020-03-31 00:03:07 +0200

    Add project files.

---
+++ tastk 1/Dog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tastk_1
+{
+   public class Dog
+    {
+
+        public string name;
+        public string race;
+        public string color;
+
+        public Dog(string aName, string aRace, string aColor)
+        {
+            name = aName;
+            race = aRace;
+            color = aColor;
+
+        }
+
+       public  string Eat()
+        {
+          return ("The dog is now eating. (dog)Mjom,mjom....");
+        }
+
+
+        public string Play()
+        {
+           return(" The dog is now playing.(dog)Wuf,wuf...!");
+        }
+
+
+        public string ChaseTail()
+        {
+            return(" Dog is now chasing its tail.(dog)Grrrrrrr..");
+        }
+
+
+    }
+
+
+}
+
+
+
+
---
+++ tastk 1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace tastk_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insert dogs name");
+            string aName = Console.ReadLine();
+
+
+
+            Console.WriteLine("Insert dogs race");
+            string aRace = Console.ReadLine();
+
+
+            Console.WriteLine("Input dogs color");
+            string aColor = Console.ReadLine();
+
+           Dog userDogCreate = new Dog(aName, aRace, aColor);
+
+            Console.WriteLine("You create this dog:");
+            Console.WriteLine($"Your dog`s name is {userDogCreate.name}.Race of the dog is {userDogCreate.race}, and his color is {userDogCreate.color}");
+
+            Console.WriteLine("What you want your dog to do:");
+            Console.WriteLine("Enter 1 - if you whant dog to eat.");
+            Console.WriteLine("Enter 2 - if ypu whant dog to play.");
+            Console.WriteLine("Enter 3 - if you whant dog to chase his tail.");
+
+
+
+            int userInput= Convert.ToInt32( Console.ReadLine());
+          
+            
+            if (userInput== 1)
+            {
+                Console.WriteLine(userDogCreate.Eat());
+
+            }
+            else if (userInput ==2)
+            {
+                Console.WriteLine(userDogCreate.Play());
+            }
+            else if (userInput == 3)
+            {
+                Console.WriteLine(userDogCreate.ChaseTail());
+            }
+           
+            
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
